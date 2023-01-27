1a60f143a9556639deb2cbd6976e8a93db23f19a
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-01-26 20:28:50 -0600

    progression for studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,14 @@
 package test;
 
+import static org.junit.Assert.fail;
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+
+    // expected, actual, delta
 
     //TODO: add tests here
     @Test
@@ -12,5 +16,32 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void hasBalancedBracketsShouldReturnTrueWhenGivenEmptyArray() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void checkIfBracketsAreReversed() {
+        String spec = "a pair of reversed brackets returns false";
+        String testData = "][";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+    @Test
+    public void singleClosingBracket() {
+        String spec = "a aingle closing bracket returns false";
+        String testData = "]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+
+    @Test
+    public void singleOpeningBracket() {
+        String spec = "a aingle opening bracket returns false";
+        String testData = "[";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
 
 }
